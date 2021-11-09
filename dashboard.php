12ffe7187267118c4cdf1b8c7cb0a115bf723c1c
ririssilvia <ririssilvia1@gmail.com>
2021-11-09 12:17:30 +0700

    update Halaman dashboard

--- dashboard.php
+++ dashboard.php
@@ -17,8 +17,8 @@ include'config.php';
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Sistem Informasi Peminjaman Barang</h1>
+          <div class="col-sm-12">
+            <h1 class="m-0 text-dark">Sistem Informasi Peminjaman Barang SMK Negeri 1 Turen</h1>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
@@ -34,22 +34,74 @@ include'config.php';
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>3</h3>
+                <h3>
+                <?php include "config.php";
+                    $result=mysqli_query($conn,"SELECT count(*) as total from barang");
+                    $data=mysqli_fetch_assoc($result);
+                    echo $data['total']; 
+                    ?>
+                </h3>
 
-                <p>Dipinjam</p>
+                <p>Barang</p>
               </div>
               <div class="icon">
                 <i class="ion ion-cube"></i>
               </div>
-              <a href="transaksi-peminjaman.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="barang.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+              <h3>
+              <?php include "config.php";
+                    $result=mysqli_query($conn,"SELECT count(*) as total from siswa");
+                    $data=mysqli_fetch_assoc($result);
+                    echo $data['total']; 
+              ?>
+              </h3>
+
+                <p>Ketua Kelas</p>
+              </div>
+              <div class="icon">
+              <i class="fas fa-user-friends"></i>
+              </div>
+              <a href="siswa.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+              <h3>
+              <?php include "config.php";
+                    $result=mysqli_query($conn,"SELECT count(*) as total FROM transaksi WHERE status = 'Pinjam'");
+                    $data=mysqli_fetch_assoc($result);
+                    echo $data['total']; 
+              ?>
+              </h3>
+
+                <p>Dipinjam</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="transaksi-peminjaman.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-              <h3>5</h3>
+              <h3>
+              <?php include "config.php";
+                    $result=mysqli_query($conn,"SELECT count(*) as total FROM transaksi WHERE status = 'Kembali'");
+                    $data=mysqli_fetch_assoc($result);
+                    echo $data['total']; 
+              ?>
+              </h3>
 
                 <p>Dikembalikan</p>
               </div>
@@ -66,7 +118,7 @@ include'config.php';
         <!-- Main row -->
         <div class="row">
           <!-- Left col -->
-          <section class="col-lg-7 connectedSortable">
+          <section class="col-lg-12 connectedSortable">
             <!-- Custom tabs (Charts with tabs)-->
             <div class="card">
               <div class="card-header">
@@ -77,7 +129,7 @@ include'config.php';
                 <div class="card-tools">
                   <ul class="nav nav-pills ml-auto">
                     <li class="nav-item">
-                      <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Drafik Laporan Bulanan</a>
+                      <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Grafik Laporan Bulanan</a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link" href="#sales-chart" data-toggle="tab">Diagram Laporan Barang</a>
@@ -102,12 +154,6 @@ include'config.php';
 
 
           </section>
-          <!-- /.Left col -->
-          <!-- right col (We are only adding the ID to make the widgets sortable)-->
-          <section class="col-lg-5 connectedSortable">
-              <div class="card-body">
-                <div id="world-map" style="height: 250px; width: 100%;"></div>
-              </div>
               <!-- /.card-body-->
               <div class="card-footer bg-transparent">
                 <div class="row">
@@ -129,58 +175,11 @@ include'config.php';
                 </div>
                 <!-- /.row -->
               </div>
-            </div>
+            </div> 
             <!-- /.card -->
 
             <!-- solid sales graph -->
-            <div class="card bg-gradient-info">
-              <div class="card-header border-0">
-                <h3 class="card-title">
-                  <i class="fas fa-th mr-1"></i>
-                  Sales Graph
-                </h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
-              </div>
-              <div class="card-body">
-                <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer bg-transparent">
-                <div class="row">
-                  <div class="col-4 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC">
-
-                    <div class="text-white">Mail-Orders</div>
-                  </div>
-                  <!-- ./col -->
-                  <div class="col-4 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC">
-
-                    <div class="text-white">Online</div>
-                  </div>
-                  <!-- ./col -->
-                  <div class="col-4 text-center">
-                    <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC">
-
-                    <div class="text-white">In-Store</div>
-                  </div>
-                  <!-- ./col -->
-                </div>
-                <!-- /.row -->
-              </div>
-              <!-- /.card-footer -->
-            </div>
+          
             <!-- /.card -->
 
             <!-- Calendar -->

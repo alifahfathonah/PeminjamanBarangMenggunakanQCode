cb8ae359eaeccafa9e3526a19fe49a7520b99b12
ririssilvia <ririssilvia1@gmail.com>
2021-12-01 15:43:08 +0700

    Api update

--- api/api_tampil.php
+++ api/api_tampil.php
@@ -1,7 +1,8 @@
 <?php
 require_once('../config.php');
+$IdBarang = $_GET['idbarang'];
 $myArray = array();
-if ($result = mysqli_query($conn, "SELECT * FROM barang  ")) {
+if ($result = mysqli_query($conn, "SELECT * FROM barang  WHERE IdBarang = '$IdBarang' " )) {
     	while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
         	$myArray[] = $row;
     	}

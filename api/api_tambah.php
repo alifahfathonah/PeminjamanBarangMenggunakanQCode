b9a8456c65100f97bb4c42580de36b751662e55a
ririssilvia <ririssilvia1@gmail.com>
2021-12-01 18:00:09 +0700

    Api Or

--- api/api_tambah.php
+++ api/api_tambah.php
@@ -1,8 +1,7 @@
 <?php
 require_once('../config.php');
 
-if (isset($_POST['IdTransaksi']) || isset($_POST['IdBarang']) || isset($_POST['Nama']) || isset($_POST['SwKelas']) || isset($_POST['BrgNama'])
-|| isset($_POST['Spesifikasi']) || isset($_POST['qty']) || isset($_POST['TglPinjam']) || isset($_POST['TglKembali']) || isset($_POST['status'])) {
+ 
 		$IdTransaksi = $_POST['IdTransaksi']; 
 		$IdBarang = $_POST['IdBarang']; 
 		$Nama = $_POST['Nama']; 
@@ -24,6 +23,4 @@ if (isset($_POST['IdTransaksi']) || isset($_POST['IdBarang']) || isset($_POST['N
 	} else {
 		echo json_encode(array('RESPONSE' => 'FAILED'));
 	}
-} else {
-	echo "GAGAL";
-}
+ 
